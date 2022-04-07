0220929687ffcf5c4a007cf9daafeb2c6fe1a89d
theodore2021 <25tkeithmahler@athenian.org>
2022-04-07 12:30:18 -0700

    day 1

--- src/print_input.py
+++ src/print_input.py
@@ -6,3 +6,19 @@
 #############################
 
 # Write code here:
+print("ay, we in megans class i know you know its hella gass")
+print("yuh, we running from the feds i know they know we got that tech")
+print("shlat, her man cant compete I know he know I got the heat")
+print("yuh, we aint on the same team cus im tryna reach that american dream")
+
+name = "darren"
+print(f"hello {name}")
+name = input("what is 2+2: ")
+print("wrong")
+x = 2
+y = 19
+print(x+y)
+
+num1 = int(input("enter a number"))
+num2 = int(input("enter another number"))
+print(num1+num2)
